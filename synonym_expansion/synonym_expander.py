eb36bec26a8c38224b7bc2c4ad43b2b3d5920b18
eddiebary <edgarmonis@gmail.com>
2020-09-02 07:19:56 +0000

    adding synonym expansion from wordnet as well as syn list

--- synonym_expansion/synonym_expander.py
+++ synonym_expansion/synonym_expander.py
@@ -20,8 +20,9 @@ class SynonymExpander:
         
         Should i vaccinate with measles ?
     """
-    # TODO : Add synonym expansion from syn_list.txt
-    def __init__(self):
+    
+    def __init__(self, use_wordnet=True,  \
+            use_synlist=False, synlist_path="./synlist"):
         """
         Setup Synonym Expander with spacy pipleline for synonym replacement
         """
@@ -30,11 +31,25 @@ class SynonymExpander:
         nlp.add_pipe(WordnetAnnotator(nlp.lang), after='tagger')
 
         self.nlp = nlp
-        self.domain_of_interest = ['person','economy']
+        self.domain_of_interest = ['person']
 
-    def expand_sentence(self, sent):
+        self.all_stopwords = nlp.Defaults.stop_words
+
+        self.use_wordnet = use_wordnet
+        self.use_synlist = use_synlist
+
+        if self.use_synlist:
+            # TODO : Add synonym expansion from syn_list.txt
+            self.word_to_name_dict , self.name_to_syn_dict = \
+                self.parse_synlist(synlist_path)
+
+
+    def expand_sentence(self, sent, debug = False):
         """
         For a given sentence, replaces a word with the entire list of synonyms
+
+        First get synonyms, create a list
+        then create a boosted query
 
         Inputs
         ------
@@ -49,19 +64,133 @@ class SynonymExpander:
         """
         # Add a synonym list
         enriched_sentence = []
-        sentence = nlp(sent)
+        sentence = self.nlp(sent)
+        # Remove stopwords
+        sentence= [word for word in sentence if not word in self.all_stopwords]
 
         # For each token in the sentence
         for token in sentence:
             # We get those synsets within the desired domains
-            synsets = token._.wordnet.wordnet_synsets_for_domain(domain_of_interest)
+            synsets = token._.wordnet.\
+                wordnet_synsets_for_domain(self.domain_of_interest)
             if not synsets:
                 enriched_sentence.append(token.text)
             else:
-                lemmas_for_synset = [lemma for s in synsets for lemma in s.lemma_names()]
+                lemmas_for_synset = [lemma for s in synsets \
+                    for lemma in s.lemma_names()]
                 # If we found a synset in the economy domains
                 # we get the variants and add them to the enriched sentence
-                enriched_sentence.append('{}'.format(' '.join(set(lemmas_for_synset))))
+                if debug:
+                    enriched_sentence.append(\
+                        '({})'.format(
+                            ('|'.join(set(lemmas_for_synset)))).replace('_',' ')\
+                        )
+                else:
+                    enriched_sentence.append(\
+                        '{}'.format(\
+                            (' '.join(set(lemmas_for_synset)))).replace('_', ' ')\
+                        )
 
         # Let's see our enriched sentence
         return ' '.join(enriched_sentence)
+    
+    def return_synonyms(self, sent):
+        """
+        For a given sentence, returns all synonyms present in it
+
+        For eg : My child is big.
+            will give
+
+            ["baby", "child", "toddler", "big" , "large"]
+
+        Inputs
+        ------
+
+        sent : String
+            The sentence which needs to be expanded into a string
+        """
+        synonyms = []
+        if self.use_wordnet:
+            # Add a synonym list
+            sentence = self.nlp(sent)
+            # Remove stopwords
+            sentence= [word for word in sentence if not word in self.all_stopwords]
+
+            # For each token in the sentence
+            for token in sentence:
+                # We get those synsets within the desired domains
+                synsets = token._.wordnet.\
+                    wordnet_synsets_for_domain(self.domain_of_interest)
+                if not synsets:
+                    continue
+                else:
+                    lemmas_for_synset = [lemma for s in synsets \
+                        for lemma in s.lemma_names()]
+                    
+                    if len(set(lemmas_for_synset)) <= 1:
+                        continue
+                    # If we found a synset in the economy domains
+                    # we get the variants and add them to the enriched sentence
+                    for x in set(lemmas_for_synset):
+                        synonyms.append( "\"{}\"".format(x.replace('_',' ')))
+            
+        if self.use_synlist:
+            sentence = self.nlp(sent)
+            for word in sentence:
+                if word.text in self.word_to_name_dict.keys():
+                    key = self.word_to_name_dict[word.text]
+                    synonyms.extend(self.name_to_syn_dict[key])
+            
+        return synonyms
+
+    def parse_synlist(self, path):
+        """
+        parse a file to create 2 dictionaries
+
+        word -> synonym group name
+        synonym group name -> all synonym group words
+        """
+
+        word_to_name_dict = {}
+        name_to_syn_dict  = {}
+
+        with open(path) as f:
+            for line in f:
+                synonyms = [word.strip() for word in line.split(',')]
+                syn_name  = synonyms[0]
+                for word in synonyms:
+                    word_to_name_dict[word] = syn_name
+                
+                syn_dict = set(synonyms)
+                name_to_syn_dict[syn_name] = syn_dict
+
+        return word_to_name_dict, name_to_syn_dict
+
+
+if __name__ == '__main__':
+    syn_exp = SynonymExpander()
+    
+    sent = "My daughter is 10 years old. What is a good vaccine ?"
+    print(syn_exp.expand_sentence(sent, debug=True))
+
+    print( "This is the model input")
+    print(syn_exp.expand_sentence(sent, debug=False))
+
+    print("Now we shall show the synonyms")
+    print('='*80)
+    print(syn_exp.return_synonyms(sent))
+
+    print( "This is the model input")
+    print(syn_exp.return_synonyms(sent))
+
+    # Try using a synlist
+    synlist_path="./syn_test.txt"
+    f = open(synlist_path, "w")
+    f.write("apple, granny smith, washington red\nboy, child, son, daughter\n")
+    f.close()
+    syn_exp_syn_list = SynonymExpander(\
+        use_synlist=True, use_wordnet=False, synlist_path=synlist_path)
+    
+    print(syn_exp_syn_list.return_synonyms("My boy likes apple."))
+    
+
